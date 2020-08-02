0e766c6604243bc45152b8271dd2300675f23b02
sekiya <tsugi0807@gmail.com>
2020-08-02 10:24:56 +0900

    Lesson5課題提出用
    ・「score」表示用に「ScoreText」のUIを作成
    ・スコアの値を表示用に「ScoreCountText」のUIを作成
    ・BallController.csにスコア表示のスクリプトを追加

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -8,10 +8,20 @@ public class BallController : MonoBehaviour {
 	private float visiblePosZ = -6.5f;
 	private GameObject gameoverText;
 
+	private GameObject scorecountText;
+	//score　スコアの値
+	private int score;
+
 
 	// Use this for initialization
 	void Start () {
 		this.gameoverText = GameObject.Find("GameOverText");
+
+		//ScoreCountTextをscorecountTextに代入
+		this.scorecountText = GameObject.Find("ScoreCountText");
+		//スコアを０にセット
+		this.score = 0;
+
 	}
 	
 	// Update is called once per frame
@@ -19,7 +29,29 @@ public class BallController : MonoBehaviour {
 		if(this.transform.position.z < this.visiblePosZ)
         {
 			this.gameoverText.GetComponent<Text>().text = "Game Over";
-        }
+			
+		}
+
+		//最新のスコアの値を表示
+		this.scorecountText.GetComponent<Text>().text = score.ToString();
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		//小さい星　10点、大きい星　20点、　雲　15点
+		if (other.gameObject.tag == "SmallStarTag")
+		{
+			this.score += 10;
+		}
+		else if (other.gameObject.tag == "LargeStarTag")
+		{
+			this.score += 20;
+		}
+		else if (other.gameObject.tag == "SmallCloudTag" || other.gameObject.tag == "LargeCloudTag")
+		{
+			this.score += 15;
+		}
 		
 	}
 }
